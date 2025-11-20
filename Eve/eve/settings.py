021bd5708f72baa32f89c91c88d5dba7e0574d24
LexLPS <alexander.pieczka@code.berlin>
2025-11-20 16:20:51 +0100

    App start

--- Eve/eve/settings.py
+++ Eve/eve/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,6 +38,15 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    # Third-party
+    "rest_framework",
+
+    # Our apps
+    "core",
+    "accounts",
+    "ecommerce",
+    "payments",
 ]
 
 MIDDLEWARE = [
@@ -73,12 +83,19 @@ WSGI_APPLICATION = 'eve.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": config("DB_NAME"),
+        "USER": config("DB_USER"),
+        "PASSWORD": config("DB_PASSWORD"),
+        "HOST": "localhost",
+        "PORT": "5432",
     }
 }
-
+MONGODB = {
+    "HOST": "mongodb://localhost:27017",
+    "DB_NAME": config("DBMON_NAME"),
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
